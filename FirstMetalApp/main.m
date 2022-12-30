78ad781a15a8f149b57f0928e6402cb23386a02b
Jakub Nowak <nowakkuba99@gmail.com>
2022-12-30 11:22:25 +0100

    First commit

--- FirstMetalApp/main.m
+++ FirstMetalApp/main.m
@@ -11,6 +11,7 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
         // X
+        // XXX
     }
     return NSApplicationMain(argc, argv);
 }
