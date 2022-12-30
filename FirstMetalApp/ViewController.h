4920d34dbb8c8c29b5833bfd99c550ec428f9bb9
Jakub Nowak <nowakkuba99@gmail.com>
2022-12-30 11:07:41 +0100

    Initial Commit

---
+++ FirstMetalApp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FirstMetalApp
+//
+//  Created by Jakub Nowak on 30/12/2022.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ FirstMetalApp/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  FirstMetalApp
+//
+//  Created by Jakub Nowak on 30/12/2022.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
