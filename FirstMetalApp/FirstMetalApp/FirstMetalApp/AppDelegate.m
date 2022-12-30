688b2dd996ebea87d4b7d90dea1dbf6c07d80b0f
Jakub Nowak <nowakkuba99@gmail.com>
2022-12-30 11:35:19 +0100

    Created new project

---
+++ FirstMetalApp/FirstMetalApp/FirstMetalApp/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  FirstMetalApp
+//
+//  Created by Jakub Nowak on 30/12/2022.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
