f3925de7febdd5f53666fe9340beb31a0be61a1c
Jakub Nowak <nowakkuba99@gmail.com>
2023-01-22 20:45:45 +0100

    First commit of a project

---
+++ LISA_Algorithm/LISA_Algorithm/src/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  LISA_Algorithm
+//
+//  Created by Jakub Nowak on 21/01/2023.
+//
+
+#include <iostream>
+#include "implot.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "👔 Hello, World!\n";
+    return 0;
+}
+
